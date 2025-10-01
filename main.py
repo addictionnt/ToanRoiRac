742d00c1931295f56440f3a7f480fa689792fd67
Thành Chính <nguyennhuthanh03042006@gmail.com>
2025-10-01 09:48:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -74,4 +74,68 @@ def kruskals_mst(V, edges):
     return cost
 
 cost = kruskals_mst(9, edges)
+
 print(cost)
+
+
+
+mport sys
+class Graph():
+
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = [[0 for column in range(vertices)]
+                      for row in range(vertices)]
+
+    def printMST(self, parent):
+        print("Edge \tWeight")
+        for i in range(1, self.V):
+            print(parent[i], "-", i, "\t", self.graph[parent[i]][i])
+
+    def minKey(self, key, mstSet):
+        min = sys.maxsize
+
+        for v in range(self.V):
+            if key[v] < min and mstSet[v] == False:
+                min = key[v]
+                min_index = v
+
+        return min_index
+    
+    
+def primMST(self):
+    key = [sys.maxsize] * self.V
+    parent = [None] * self.V
+    key[0] = 0
+    mstSet = [False] * self.V
+    parent[0] = -1 # First node is always the root of
+    for count in range(self.V):
+        u = self.minKey(key, mstSet)
+        mstSet[u] = True
+
+        for v in range(self.V):
+            if self.graph[u][v] > 0 and mstSet[v] == \
+               False and key[v] > self.graph[u][v]:
+                key[v] = self.graph[u][v]
+                parent[v] = u
+
+    self.printMST(parent)
+    
+    # Tính toán tổng trọng số và tập cạnh
+    total_weight = 0
+    mst_edges = []
+    # Lặp qua mảng key/parent để tính tổng trọng số và xây dựng tập cạnh
+    for i in range(1, self.V):
+        # Trọng số của cạnh nối i vào cây
+        weight = key[i]
+        total_weight += weight
+        
+        # Cạnh là (đỉnh cha, đỉnh con)
+        edge = (parent[i], i)
+        mst_edges.append(edge)
+
+    # In ra các cạnh (sử dụng hàm đã có)
+    self.printMST(parent)
+    
+    # TRẢ VỀ: (tổng trọng số, danh sách các cạnh)
+    return total_weight, mst_edges
